401fb7a28662fb814a5a4f035303d77c35436f51
野村直弘 <naohiro@nomuranaohiro-no-MacBook-Pro.local>
2015-03-10 10:45:10 +0900

    ワイヤー追加 あと、マップの読み込みエラーあり。。

--- Player.cpp
+++ Player.cpp
@@ -7,7 +7,9 @@
 //
 
 #include "Player.h"
-#define CTS(x) #x
+#include "LayerTitle.h"
+
+TMXTiledMap *map;
 
 USING_NS_CC;
 
@@ -27,8 +29,8 @@ bool Player::init(){
     for (int i = 0; i < 21; i++) {ani[i] = Animation::create(); }
     Texture2D* texture = TextureCache::sharedTextureCache()->addImage("neko.png");
     int anime[] = { 4, 4, 4, 4, 6, 6, 3, 3, 4, 1, 1 };
-    for (int y = 0; y < 21; y++){
-        int n = y <= 10 ? y : y - 10;
+    for (int y = 0; y < (int)STATE::END; y++){
+        int n = y < 10 ? y : y - 10;
         for (int x = 0; x < anime[n]; x++){
             Rect rect = Rect(x * size, y * size, size, size);
             ani[y]->addSpriteFrameWithTexture(texture, rect);
@@ -44,6 +46,12 @@ bool Player::init(){
     
     AnimationRun(strAnimeName[(int)STATE::IDLE]);
     
+    //this->runAction(Follow::create(player));
+    
+    map = TMXTiledMap::create("samplemap.tmx");
+    map->setScale(2);
+    this->addChild(map);
+    
     scheduleUpdate();
     return true;
 }
@@ -51,40 +59,152 @@ bool Player::init(){
 void Player::update(float delta){
     int n = (int)dir > 0 ? 0 : 10;
     if (state != oldState || dir != oldDir){
+        if (state != STATE::JUMPUP && oldState == STATE::JUMPDOWN){
+            velocity = Vec2::ZERO;
+        }
+        AnimationRun(strAnimeName[(int)state+n]);
         oldState = state;
         oldDir = dir;
-        AnimationRun(strAnimeName[(int)state+n]);
     }
     
     player->setPosition(player->getPosition() + velocity);
-    velocity.y-=0.2f;
-    player->setPositionY(fmax(miny, player->getPosition().y));
+    Vec2 gravity = Vec2(0, -0.2f);
+    velocity += gravity;
+    //player->setPositionY(fmax(miny, player->getPosition().y));
 
-    if (player->getPosition().y > miny){
+    Vec2 pos = player->getPosition();
+    
+    removeChildByTag(99);
+    if (pos.y > miny){
         state = velocity.y > 0 ? STATE::JUMPUP : STATE::JUMPDOWN;
+        if (wire){
+            auto w = DrawNode::create();
+            w->drawSegment(pos, standPos, 1, Color4F::WHITE);
+            addChild(w, 0, 99);
+            float leng = Length(standPos - pos);
+            Vec2 vector = Vec2(pos.x - standPos.x, pos.y - standPos.y);
+            Vec2 unit = Normalize(vector);
+            velocity = Vertical(velocity + Vertical(gravity, vector), vector);
+            pos = standPos + unit * leng;
+        }
     }else{
+        wire = false;
         if (velocity.x != 0){
             state=STATE::RUN;
         }else{
             state=STATE::IDLE;
         }
     }
+    
+    if (CollisionBox(player, map)){
+        velocity = Vec2::ZERO;
+    }
+    
+//    TMXLayer *collisionLayer = map->layerNamed("collision");
+//    for (int y = 0; map->getMapSize().height; y++){
+//        for (int x = 0; x < map->getMapSize().width; x++){
+//            Sprite *col = collisionLayer->getTileAt(Vec2(x, y));
+//            if (col){
+//                Vec2 p = tileCoordinateToPos(map, 30, Vec2(x, y));
+//                if (AABB(p, pos)){
+//                    
+//                }
+//            }
+//        }
+//    }
 }
 
 bool Player::onTouchBegan(Touch* touch, Event* event){
     standPos = touch->getLocation();
+    
+    Vec2 screen = Director::getInstance()->getVisibleSize();
+    if (standPos.y > screen.y / 2){
+        wire = true;
+    }
     return true;
 }
 
 void Player::onTouchMoved(Touch* touch, Event* event){
-    velocity.x = (touch->getLocation().x - standPos.x) > 0 ? 1 : -1;
-    dir = (DIR)velocity.x;
-    velocity.x *= 3;
+    if (!wire){
+        velocity.x = (touch->getLocation().x - standPos.x) > 0 ? 1 : -1;
+        dir = (DIR)velocity.x;
+        velocity.x *= 3;
+    }
 }
 
 void Player::onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event){
-    velocity.x = 0;
-    if (player->getPosition().y == miny) velocity.y = 10;
+    if (!wire){
+        velocity.x = 0;
+        if (player->getPosition().y == miny) velocity.y = 10;
+    }
+    wire = false;
+}
+
+bool Player::CollisionBox(Sprite *p, TMXTiledMap *m){
+    Vec2 v = p->getPosition();
+    TMXLayer *collisionLayer = m->layerNamed("collision");
+    //collisionLayer->setVisible(false);
+    std::vector<Sprite*> block;
+    for (int y = 0; y < m->getTileSize().height; y++){
+        for (int x = 0; x < m->getTileSize().width; x++){
+            Vec2 ve =Vec2(x, y);
+            addPhysicsBody(collisionLayer, ve);
+//            block.push_back(collisionLayer->getTileAt(Vec2(x, y)));
+        }
+    }
+    for (auto &b : block){
+        if (p->boundingBox().intersectsRect(b->boundingBox())){
+            return true;
+        }
+    }
+    return false;
+}
+Sprite* Player::addPhysicsBody(cocos2d::TMXLayer *layer, cocos2d::Vec2 &coordinate)
+{
+    //タイルのスプライトを取り出す
+    auto sprite = layer->getTileAt(coordinate);
+    if(sprite){
+        //剛体用のマテリアルを作成
+        auto material = PhysicsMaterial();
+        //摩擦ゼロ
+        material.friction = 0;
+        //剛体を設置
+        auto physicsBody = PhysicsBody::createBox(sprite->getContentSize(), material);
+        //剛体を固定
+        physicsBody->setDynamic(false);
+        //剛体をつけるSpriteのアンカーポイントを中心に
+        map->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+        auto gid = layer->getTileGIDAt(coordinate);
+        auto properties = map->getPropertiesForGID(gid).asValueMap();
+        if (properties.count("collision")>0){
+            auto category = properties.at("collision").asInt();
+            physicsBody->setCategoryBitmask(category);
+//            physicsBody->setContactTestBitmask(static_cast<int>(TileType::PLAYER));
+//            physicsBody->setCollisionBitmask(static_cast<int>(TileType::));
+        }
+        //剛体にSpriteをつける
+        map->setPhysicsBody(physicsBody);
+        
+        return sprite;
+        
+    }
+    return nullptr;
+}
+Vec2 Player::tileCoordinateToPos(TMXTiledMap *m, Size s, Vec2 p){
+    float size = 2;
+    float x = floor(s.width/2 * size + p.x * m->getTileSize().width * size);
+    float y = floor(s.height/2 * size + p.y * m->getTileSize().height * size);
+    return Vec2(x, y);
+}
+
+Vec2 Player::posToTileCoordinate(TMXTiledMap *m, Size s, Vec2 p){
+    float size = 2;
+    float x = floor((p.x - s.width/2 * size) / (m->getTileSize().width * size));
+    float y = floor((p.y - s.height/2 * size) / (m->getTileSize().height * size));
+    return Vec2(x, y);
+}
+
+void Player::WireAction(){
 }
 
 void Player::initTouch(){
@@ -102,4 +222,3 @@ void Player::AnimationRun(std::string name){
     RepeatForever *action = RepeatForever::create( Animate::create(animation) );
     player->runAction(action);
 }
-
--- Player.h
+++ Player.h
@@ -10,10 +10,14 @@
 #define __PlayGame__Player__
 
 #include <stdio.h>
+#include "Box2D/Box2D.h"
+#include "GL-ESRender.h"
+
+#define PTM_RATIO 32.0
 USING_NS_CC;
 
 class Player : public cocos2d::Layer {
-public:
+private:
 //    enum STATE{
 //        R_IDLE, R_IDLE2, R_ATTACK, R_HOLD, R_RUN,
 //        R_HOLDRUN, R_JUMPUP, R_JUMPDOWN, R_CLIMB, R_DAMAGE,
@@ -34,6 +38,7 @@ public:
         "L_HoldRun", "L_JumpUp", "L_JumpDown", "L_Climb", "L_Damage", "Die"
     };
 
+    bool wire = false;
     float miny;
     DIR dir, oldDir;
     STATE state, oldState;
@@ -43,10 +48,36 @@ public:
     void AnimationRun(std::string name);
     void update(float delta);
     void initTouch();
+    void WireAction();
     virtual bool init();
     virtual bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
     virtual void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event);
     virtual void onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event);
+    
+    float Length(Vec2 v){ return sqrtf(v.x * v.x + v.y * v.y); }
+    Vec2 Normalize(Vec2 v){
+        float l = Length(v);
+        return Vec2(v.x / l, v.y / l);
+    }
+    float Dot(Vec2 a, Vec2 b){ return a.x * b.x + a.y * b.y; }
+    Vec2 Vertical(Vec2 origin, Vec2 v){
+        Vec2 unitV = Normalize(v);
+        float vLength = Dot(origin, unitV);
+        unitV *= vLength;
+        return Vec2(origin.x - unitV.x, origin.y - unitV.y);
+    }
+    bool AABB(Vec2 pos, Vec2 pos2, Vec2 size, Vec2 size2){
+        Vec2 p1 = pos + size, p2 = pos2 + size2;
+        if (pos.x < p2.x && pos2.x < p1.x && pos.y < p2.y && pos2.y < p1.y){
+            return true;
+        }
+        return false;
+    }
+    Vec2 posToTileCoordinate(TMXTiledMap *m, Size s, Point point);
+    Vec2 tileCoordinateToPos(TMXTiledMap *m, Size s, Point point);
+    bool CollisionBox(Sprite *p, TMXTiledMap *m);
+    Sprite* addPhysicsBody(cocos2d::TMXLayer *layer, cocos2d::Vec2 &coordinate);
+public:
     CREATE_FUNC(Player);
 };
 
